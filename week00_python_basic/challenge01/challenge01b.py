fc49065eadf939ccbed8b14876d99b1704a02eaa
Trang <trangtruong16101993@gmail.com>
2019-10-07 11:46:05 +0700

    make code challenge01b unittest-compatible

--- week00_python_basic/challenge01/challenge01b.py
+++ week00_python_basic/challenge01/challenge01b.py
@@ -1,27 +1,29 @@
-l = []
-with open("input01b.txt", "r") as f1:
-    # Read next line
-    n = int(f1.readline())
-    line = f1.readline()
-    # check line is not empty
-    while line:
-        l.append(float (line.strip()))
-        print(line.strip())
+def max_claim(input, output):
+    l = []
+    with open(input, "r") as f1:
+        # Read next line
+        n = int(f1.readline())
         line = f1.readline()
-    print(l)
-f1.close()
+        # check line is not empty
+        while line:
+            l.append(float(line.strip()))
+            print(line.strip())
+            line = f1.readline()
+        print(l)
+    f1.close()
 
-is_max = []
-for i in range(n):
-    if l[i] == max(l):
-        is_max.append(1)
-    else:
-        is_max.append(0)
-print(is_max)
+    is_max = []
+    for i in range(n):
+        if l[i] == max(l):
+            is_max.append(1)
+        else:
+            is_max.append(0)
+    print(is_max)
 
-#ghi vao file output
-f2 = open('output01b.txt', 'w+')
-for i in range(n):
-    f2.write(f'{is_max[i]}\n')
-f2.close()
+    # ghi vao file output
+    f2 = open(output, 'w+')
+    for i in range(n):
+        f2.write(f'{is_max[i]}\n')
+    f2.close()
 
+max_claim(input='input01b.txt', output='output01b.txt')
