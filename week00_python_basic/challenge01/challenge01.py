4d9ebdb610d6ee7de2e769214eaf8fc930c723ca
thangtm <thangtm589@gmail.com>
2019-10-08 14:38:36 +0700

    challenge01a

--- week00_python_basic/challenge01/challenge01.py
+++ week00_python_basic/challenge01/challenge01.py
@@ -1,23 +1,42 @@
+import os
+
 
 def find_min(input, output):
-    # Open file
-    l = []
-    with open(input, "r") as f1:
-        # Read next line
-        line = f1.readline()
-        # check line is not empty
-        while line:
-            v = float(line.strip())  # v aka value
-            l.append(v)
-            print(line.strip())
-            line = f1.readline()
-    f1.close()
+    if os.path.isfile(input):
+        # Read file
+        with open(input, "r") as f1:
+            data = [line.rstrip('\n') for line in f1.readlines()]
+    # tc00 pass
+    else:
+        raise Exception(f'File {input} not found')
 
-    f = open(output, 'w')
-    f.write(f'Day la so nho nhat: {min(l)} ')
-    print(min(l))
-    f.close()
+    # tc03c pass
+    if os.path.getsize(input) == 0:
+        raise Exception('Invalid input: Empty file')
+    # tc03b pass
+    elif all(s == '' or s.isspace() for s in data):
+        raise Exception('Invalid input: Empty file with empty String')
+    # tc02 pass
+    elif len(data) != 6:
+        raise Exception('Invalid input: List of numbers should have 6 numbers')
 
-find_min(input='input01.txt', output='output01.txt')
+    else:
+        try:
+            data = [float(n.rstrip(', ')) for n in data]
+            # Write to file
+            f = open(output, 'w')
+            f.write(f'{int(min(data))}') # tc01
+            f.close()
+        except:
+            raise Exception('Invalid input: The item in the list must be a number')
 
 
+
+if __name__ == '__main__':
+    find_min(input='input01.txt', output='output01.txt')
+
+
+
+    # tc03a pass
+    # elif all(x.strip(', ').isdigit() is False for x in data):
+    #     raise Exception('Invalid input: The item in the list must be a number')
